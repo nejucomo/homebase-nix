52b8761ae947b588b8139192475077ce6bac1dd3
Nate Wilcox <nejucomo+dev@gmail.com>
2021-02-21 03:34:44 +0000

    Wrapped vim proof-of-concept.

--- default.nix
+++ default.nix
@@ -2,11 +2,12 @@ let
   nixpkgs = import <nixpkgs> {};
 
   pname = builtins.baseNameOf ./.;
+  vim = import ./wrapped-vim;
 in
   nixpkgs.symlinkJoin {
     name = "${pname}-0.1";
-    paths = with nixpkgs; [
-      tmux
+    paths = [
+      nixpkgs.tmux
       vim
     ];
   }
