bdec191fa29987bf437091a96a61722d97021441
Nate Wilcox <nejucomo+dev@gmail.com>
2021-02-22 19:08:08 +0000

    basic config

--- default.nix
+++ default.nix
@@ -4,25 +4,24 @@ let
   pname = builtins.baseNameOf ./.;
   version = "0.1";
 
+  pkgsVanilla = with nixpkgs; [
+    alacritty
+    tmux
+  ];
+
   wrapBin = import ./wrapBin.nix;
+  pkgsWrapped = [
+    (wrapBin {
+      pkg = "vim";
+      wrapArgs = [ "-u" "${./config/vimrc}" ];
+    })
+    (wrapBin {
+      pkg = "herbstluftwm";
+      wrapArgs = [ "--autostart" "${./config/herbstluftwm-autostart}" ];
+    })
+  ];
 in
   nixpkgs.symlinkJoin {
     name = "${pname}-0.1";
-    paths = [
-      nixpkgs.tmux
-      (wrapBin {
-        pkg = "vim";
-        wrapArgs = [
-          "-u"
-          "${./config/vimrc}"
-        ];
-      })
-      (wrapBin {
-        pkg = "herbstluftwm";
-        wrapArgs = [
-          "--autostart"
-          "${./config/herbstluftwm-autostart}"
-        ];
-      })
-    ];
+    paths = pkgsVanilla ++ pkgsWrapped;
   }
