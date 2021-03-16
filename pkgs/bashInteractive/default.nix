a8f2056320d8593c84056f8f598d55db267a362c
Nate Wilcox <nejucomo+dev@gmail.com>
2021-03-16 08:59:12 -0700

    Wrap /bin/sh shebangs; document bashrc trickiness with startx.

--- pkgs/bashInteractive/default.nix
+++ pkgs/bashInteractive/default.nix
@@ -1,6 +1,9 @@
-{ wrapBins, ... }: wrapBins {
+{ wrapBins, ... }: wrapBins rec {
+
   bash = { realbin, ... }: ''
     #!/bin/sh
     exec "${realbin}" "--rcfile" "${./bashrc}" "$@"
   '';
+
+  sh = bash;
 }
