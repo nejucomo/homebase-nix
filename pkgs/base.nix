620d1313ebddbb53ee592a5ee36521e264e6d3d6
nsys <nobody@nowhere>
2023-11-23 14:21:13 -0700

    Include man page outputs if present.

--- pkgs/base.nix
+++ pkgs/base.nix
@@ -1,26 +1,44 @@
-with (import <nixpkgs> {});
-[
-  acpi
-  coreutils
-  dunst.man
-  findutils
-  firefox
-  gawk
-  gnugrep
-  gnused
-  gzip
-  helix
-  i3lock
-  killall
-  less
-  man
-  nix
-  libnotify
-  ps
-  pstree
-  ripgrep
-  unclutter
-  which
-  xorg.xsetroot
-  xss-lock
-] 
+let
+  nixpkgs = import <nixpkgs> {};
+
+  basepkgs = with nixpkgs; [
+    acpi
+    coreutils
+    findutils
+    firefox
+    gawk
+    gnugrep
+    gnused
+    gzip
+    helix
+    i3lock
+    killall
+    less
+    man
+    nix
+    libnotify
+    ps
+    pstree
+    ripgrep
+    unclutter
+    which
+    xorg.xsetroot
+    xss-lock
+  ];
+
+  cookedpkgs =
+    let
+      flatten = nixpkgs.lib.lists.flatten;
+      cook = pkg:
+        if pkg ? man
+        then [pkg pkg.man]
+        else [pkg];
+    in
+      flatten (map cook basepkgs);
+
+  manonly = map (pkg: pkg.man) (with nixpkgs; [
+    dunst
+    herbstluftwm
+  ]);
+in
+  cookedpkgs ++ manonly
