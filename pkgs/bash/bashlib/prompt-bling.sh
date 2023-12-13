aafb81d9d0f4e4d8ecfa7b5def46ff0e3a176004
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-12 18:42:04 -0800

    Add nesting level to prompt bling.

---
+++ pkgs/bash/bashlib/prompt-bling.sh
@@ -0,0 +1,44 @@
+export PS1='\n\n  '
+
+function prompt_append
+{
+  local control="$1"
+  local frag="$2"
+
+  PS1+='\033['
+  case "$control" in
+    bg) PS1+='35' ;; # magenta
+    norm) PS1+='33' ;; # yellow
+    green) PS1+='1;32' ;; # bright green
+    off) PS1+='0' ;; # off
+    *) PS1+='1;31' ;; # bright red
+  esac
+    
+  PS1+='m'
+  PS1+="$frag"
+  ps1+='\033[0m'
+}
+
+band=''
+if (( $HOMEBASE_NEST_LEVEL > 1 ))
+then
+  band="$(printf '%0.s=' $(seq 2 $HOMEBASE_NEST_LEVEL))"
+fi
+
+prompt_append bg '---'
+prompt_append green "$band"
+prompt_append bg '{ '
+prompt_append norm '\$?=$?'
+prompt_append bg ' ; '
+prompt_append norm '\u'
+prompt_append bg ' @ '
+prompt_append norm '\h'
+prompt_append bg ' : '
+prompt_append norm '\w'
+prompt_append bg ' }'
+prompt_append green "$band"
+prompt_append bg '---'
+prompt_append off '\n\$ '
+
+unset prompt_append
+unset band
--- pkgs/bash/bashlib/settings.sh
+++ pkgs/bash/bashlib/settings.sh
@@ -1,4 +1,5 @@
 set -o vi
 
-export PS1='\n\n  \033[35m---{ \033[33m\$?=$? \033[35m; \033[33m\u \033[35m@ \033[33m\h \033[35m: \033[33m\w \033[35m}---\033[0m\n\$ '
+export HOMEBASE_NEST_LEVEL=$(( "${HOMEBASE_NEST_LEVEL:-0}" + 1 ))
+export NIX_SHELL_PRESERVE_PROMPT='1'
 export EDITOR='vim'
