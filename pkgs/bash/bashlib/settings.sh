80fc39c907d63ef9d24cb13c60dc5c4c5f1ba88f
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-21 06:06:49 -0800

    `GTK_THEME=Adwaita:dark`

--- pkgs/bash/bashlib/settings.sh
+++ pkgs/bash/bashlib/settings.sh
@@ -2,5 +2,6 @@ set -o vi
 
 export EDITOR='vim'
 export HOMEBASE_NEST_LEVEL=$(( "${HOMEBASE_NEST_LEVEL:-0}" + 1 ))
+export GTK_THEME='Adwaita:dark'
 export NIX_INDEX_DATABASE='/usr/local/usershare/knack/nix-index'
 export NIX_SHELL_PRESERVE_PROMPT='1'
