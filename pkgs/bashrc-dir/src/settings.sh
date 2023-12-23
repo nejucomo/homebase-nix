4aa828813ea9011a4d7d52205e769fa347cff3bc
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-22 21:46:46 -0800

    Add ~/.cargo/bin to PATH.

--- pkgs/bashrc-dir/src/settings.sh
+++ pkgs/bashrc-dir/src/settings.sh
@@ -5,3 +5,4 @@ export HOMEBASE_NEST_LEVEL=$(( "${HOMEBASE_NEST_LEVEL:-0}" + 1 ))
 export GTK_THEME='Adwaita:dark'
 export NIX_INDEX_DATABASE='/usr/local/usershare/knack/nix-index'
 export NIX_SHELL_PRESERVE_PROMPT='1'
+export PATH="$HOME/.cargo/bin:$PATH"
