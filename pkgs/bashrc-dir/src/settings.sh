30718d0598ccfce695fb165ab5a5f0fdfe6acf30
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-28 09:54:11 -0800

    Set `RUSTFLAGS` and `RUSTDOCFLAGS` as synonyms.

--- pkgs/bashrc-dir/src/settings.sh
+++ pkgs/bashrc-dir/src/settings.sh
@@ -6,4 +6,5 @@ export HOMEBASE_NEST_LEVEL=$(( "${HOMEBASE_NEST_LEVEL:-0}" + 1 ))
 export NIX_INDEX_DATABASE='/usr/local/usershare/knack/nix-index'
 export NIX_SHELL_PRESERVE_PROMPT='1'
 export PATH="$HOME/.cargo/bin:$PATH"
-export RUSTDOCFLAGS='-D warnings'
+export RUSTFLAGS='-D warnings'
+export RUSTDOCFLAGS="$RUSTFLAGS"
