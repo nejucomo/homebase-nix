d9a2e8d7a399b307738b78c0a68f7707d79d1f7e
Nate Wilcox <nejucomo+dev@gmail.com>
2024-02-06 09:44:37 -0800

    `bash-functions`: fix a bug in `git-clone-canonical`: pass through if not simple `<cmd> <url>` execution.

--- pkgs/bashrc-dir/src/functions.sh
+++ pkgs/bashrc-dir/src/functions.sh
@@ -8,7 +8,12 @@ function source-optional
 
 function git-clone-canonical
 {
-  local url="$1"
-  command git-clone-canonical "$url" && \
-    cd "$(command git-clone-canonical --show-path "$url")"
+  if [ "$#" -ne 1 ] || [[ "${1:--}" =~ ^- ]]
+  then
+    command git-clone-canonical "$@"
+  else
+    local url="$1"
+    command git-clone-canonical "$url" && \
+      cd "$(command git-clone-canonical --show-path "$url")"
+  fi
 }
