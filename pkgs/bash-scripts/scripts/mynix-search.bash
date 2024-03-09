deae6c65dff0c3ed1f31d8342642513068232ff7
Nate Wilcox <nejucomo+dev@gmail.com>
2024-03-08 17:20:34 -0800

    New pretty output `mynix-search`.

--- pkgs/bash-scripts/scripts/mynix-search.bash
+++ pkgs/bash-scripts/scripts/mynix-search.bash
@@ -1,3 +1,7 @@
+JQ_EXPR='to_entries | .[] | "\(.value.pname) \(.value.version) \(.key) \(.value.description)"'
+
 function main {
-  exec nix search nixpkgs "$@"
+  exec nix search --json nixpkgs "$@" \
+    | jq -r "$JQ_EXPR" \
+    | column --table --table-columns-limit 4
 }
