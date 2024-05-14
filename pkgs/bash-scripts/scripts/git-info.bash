5d0f016f1225200133cadfdba29c10bc46eef36f
Nate Wilcox <nejucomo+dev@gmail.com>
2024-05-14 14:13:43 -0700

    `git info`: polishing the output format.

--- pkgs/bash-scripts/scripts/git-info.bash
+++ pkgs/bash-scripts/scripts/git-info.bash
@@ -2,9 +2,9 @@ function main
 {
   git rev-parse --is-inside-work-tree > /dev/null 
   
-	echo ">  branch: $(git current-branch)"
+	echo "-> branch: $(git current-branch)"
 
 	git glog -5 | sed 's/^/ /' || true
 
-  git status --porcelain
+  git status --porcelain 2>&1 | sed 's/^fatal:/!!/'
 }
