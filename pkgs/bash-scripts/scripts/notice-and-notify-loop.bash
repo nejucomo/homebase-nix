cc07c445e5943d558870efcd2ff6f445d81e2a1e
Nate Wilcox <nejucomo+dev@gmail.com>
2024-05-26 22:03:13 -0700

    `notice-and-notify-loop`: intro; untested

---
+++ pkgs/bash-scripts/scripts/notice-and-notify-loop.bash
@@ -0,0 +1,42 @@
+INTERVAL="${INTERVAL:-7}"
+
+POWER_SUPPLY_THRESHOLD="${POWER_SUPPLY_THRESHOLD:-20}"
+
+POWER_SUPPLY_BASE='/sys/class/power_supply'
+
+RGX_DISCHARGING='^POWER_SUPPLY_STATUS=Discharging$'
+
+
+function main
+{
+  parse-args '' "$@"
+
+  set -s
+
+  while sleep "$INTERVAL"
+  do
+    notice-and-notify
+  done
+}
+
+function notice-and-notify
+{
+  for supply in $(power-supplies-discharging)
+  do
+    capacity="$(< "$supply" | grep '^POWER_SUPPLY_CAPACITY=' | sed 's/^.*=//')"
+    supply_name="$(echo "$supply" | sed "s|^$POWER_SUPPLY_BASE/||; s|/uevent||")"
+
+    if $(( $capacity <= $POWER_SUPPLY_THRESHOLD ))
+    then
+      notify-send \
+        --urgency='critical' \
+        --expire-time="$INTERVAL" \
+        "${supply_name} low capacity"        
+    fi
+  done
+}
+
+function power-supplies-discharging
+{
+  rg --files-with-matches "$RGX_DISCHARGING" "$POWER_SUPPLY_BASE"/*/uevent
+}
