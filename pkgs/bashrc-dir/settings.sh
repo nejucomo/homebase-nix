fe2bb3c6ba7a12f37057228e74fb9962dc8aaf4c
Nate Wilcox <nejucomo+dev@gmail.com>
2025-02-07 08:22:38 -0800

    Disable sccache.

--- pkgs/bashrc-dir/settings.sh
+++ pkgs/bashrc-dir/settings.sh
@@ -9,6 +9,7 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export RUSTFLAGS='-D warnings'
 export RUSTDOCFLAGS="$RUSTFLAGS"
 
+# sccache disabled due to a race condition(?)
 # Disable incremental builds and rely solely on `sccache`:
-export CARGO_INCREMENTAL='false'
-export RUSTC_WRAPPER="$(which sccache)"
+# export CARGO_INCREMENTAL='false'
+# export RUSTC_WRAPPER="$(which sccache)"
