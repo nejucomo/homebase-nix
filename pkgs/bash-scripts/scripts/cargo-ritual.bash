05aa46c1518a30e99166a4801abfcc3efc05672f
Nate Wilcox <nejucomo+dev@gmail.com>
2025-03-13 13:50:53 -0700

    Introduce `cargo-ritual`.

---
+++ pkgs/bash-scripts/scripts/cargo-ritual.bash
@@ -0,0 +1,19 @@
+function main {
+  while [ $# -gt 0 ]
+  do
+    ritual "$1"
+  done
+}
+
+function ritual {
+  parse-args 'path' "$@"
+  local crate="$(echo "$path" | sed 's|^\./||; s|/$||; s|/|-|g')"
+
+  set -x
+  cargo check -p "$crate"
+  cargo doc -p "$crate" --open
+  cargo test -p "$crate"
+  cargo fmt -p "$crate" -- --check
+  set +x
+}
+
