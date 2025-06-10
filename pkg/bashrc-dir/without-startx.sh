ebf0c1e86490fa4c49f08d8fd7213e146ddf5c39
nobody <nobody@nowhere.net>
2025-06-10 09:58:32 -0600

    `prompt-bling` and rc support for `nix develop` shells.

--- pkg/bashrc-dir/without-startx.sh
+++ pkg/bashrc-dir/without-startx.sh
@@ -1,5 +1,6 @@
 source "$HOMEBASE_BASHLIB/functions.sh"
 source-optional ~/.nix-profile/etc/profile.d/nix.sh
+source "$HOMEBASE_BASHLIB/settings-develop.sh"
 source "$HOMEBASE_BASHLIB/settings.sh"
 source "$HOMEBASE_BASHLIB/prompt-bling.sh"
 source-optional ~/.bashrc-user
