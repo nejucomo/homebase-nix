9135d444d2677c24f38735f87aec20f4e1f39b45
nobody <nobody@nowhere.net>
2025-07-31 21:41:53 -0700

    Pin `nixpkgs` by git ref `25.05`.

--- pkg/bashrc-dir/settings.sh
+++ pkg/bashrc-dir/settings.sh
@@ -1,8 +1,10 @@
-export NIX_INDEX_DATABASE='/usr/local/usershare/knack/nix-index'
-export NIX_SHELL_PRESERVE_PROMPT='1'
 export PATH="$HOME/.cargo/bin:$PATH"
-export RUSTFLAGS='-D warnings'
+
+export NIX_INDEX_DATABASE='/usr/local/usershare/knack/nix-index'
+export NIX_REGISTRY='nixpkgs=github:NixOS/nixpkgs/25.05'
+export NIX_SHELL_PRESERVE_PROMPT='1'
 export RUSTDOCFLAGS="$RUSTFLAGS"
+export RUSTFLAGS='-D warnings'
 
 # sccache disabled due to a race condition(?)
 # Disable incremental builds and rely solely on `sccache`:
