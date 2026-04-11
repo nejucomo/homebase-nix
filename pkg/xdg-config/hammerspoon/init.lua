bb8eff95904635e61d9b39e53bc1e3fac35a42e0
nobody <nobody@nowhere.net>
2026-04-10 21:46:55 -0600

    Add nice hammerspoon space labeling.

--- pkg/xdg-config/hammerspoon/init.lua
+++ pkg/xdg-config/hammerspoon/init.lua
@@ -11,12 +11,39 @@ hs.hotkey.bind({"ctrl", "cmd"}, "s", function()
     -- end
 end)
 
-_G.SpaceNames = {}
-_G.SpaceLabel = hs.menubar.new();
-
-hs.spaces.watcher.new(
-    function (sp)
-        local spid = hs.spaces.focusedSpace()
-        _G.SpaceLabel:setTitle(_G.SpaceNames[spid] or string.format("<space %s>", spid))
+-- Space labels!
+local function refresh_space_label_widget (spid)
+    if spid == nil then
+        spid = hs.spaces.focusedSpace()
     end
-):start()
+
+    _G.SpaceLabelWidget:setTitle(_G.SpaceLabels[spid] or string.format("<space %s>", spid))
+end
+
+_G.SpaceLabels = {}
+_G.SpaceLabelWidget = hs.menubar.new();
+
+_G.SpaceLabelWidget:setClickCallback(function ()
+    local button, newlabel = hs.dialog.textPrompt(
+      "Rename Space",
+      "Enter new space label:",
+      _G.SpaceLabels[hs.spaces.focusedSpace()] or "",
+      "OK",
+      "Cancel"
+    )
+
+    if button == "OK" then
+        _G.set_space_label(newlabel)
+    end
+end)
+
+-- Define this for ease of use from the console:
+_G.set_space_label = function (name)
+    local spid = hs.spaces.focusedSpace();
+    _G.SpaceLabels[spid] = name
+    refresh_space_label_widget(spid)
+end
+
+hs.spaces.watcher.new(function () refresh_space_label_widget(); end):start()
+
+refresh_space_label_widget()
